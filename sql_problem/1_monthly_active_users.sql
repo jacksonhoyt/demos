f67c710038e99fba599108b6e3612967c35bd1b9
Jackson Hoyt <jackson.t.hoyt@gmail.com>
2024-06-22 14:59:03 -0500

    Removing scratch work, consolidated everything into user_activity_summary.sql

--- sql_problem/1_monthly_active_users.sql
+++
@@ -1,9 +0,0 @@
-use database testdb;
-use schema lennar;
-
-
-SELECT DATE_TRUNC('month', activity_timestamp) activity_month, count(distinct user_id) active_user_cnt
-FROM user_activity_log
-GROUP BY 1
-ORDER BY 1 DESC
-;
