67da0b7962fc22bec80a91fa4015577e86da3760
Jackson Hoyt <jackson.t.hoyt@gmail.com>
2024-06-22 03:11:57 -0500

    Testing changes for each part

--- sql_problem/2_retention_rate.sql
+++ sql_problem/2_retention_rate.sql
@@ -2,154 +2,55 @@ use database testdb;
 use schema lennar;
 
 
-WITH user_activity AS (    
-    SELECT DISTINCT user_id, activity_timestamp
-    FROM user_activity_log
+with irst_month_activity as (    
+    -- identify user's first active month
+    select user_id
+    , min(activity_timestamp) first_month_activity_ts
+    , to_varchar(date_trunc('month',first_month_activity_ts), 'yyyy-mm') first_month_activity_ym
+
+    from user_activity_log
+    group by 1
 ),
 
-new_users_first_month AS (    
-    SELECT user_id, MIN(activity_timestamp) first_active_month
-    FROM user_activity
-    GROUP BY 1
+second_month_activity as (    
+    -- identify user's second active month
+    -- by excluding activities from user's first month
+    select a.user_id
+    , min(activity_timestamp) second_month_activity_ts 
+    , to_varchar(date_trunc('month',second_month_activity_ts), 'yyyy-mm') second_month_activity_ym
+    
+    from user_activity_log a                                            
+    inner join first_month_activity b on a.user_id = b.user_id and extract('month',a.activity_timestamp) != extract('month', b.first_month_activity_ts)
+    group by 1
 ),
 
-new_users_second_month AS (    
-    SELECT a.user_id, MIN(activity_timestamp) second_active_month
-    FROM user_activity a
-    INNER JOIN new_users_first_month b ON a.user_id = b.user_id AND EXTRACT('month',a.activity_timestamp) != EXTRACT('month', b.first_active_month)
-    GROUP BY 1
-),
+user_retention as (    
+    ------------------------------------------------------------------------------------------------------------
+    -- user_id: a single user
+    -- first_month_activity_ym: first year-month of user's activity
+    -- second_month_activity_ym: second year-month of user's activity
+    -- user_retained_flag: 1=user retained, 0=user not retained
+    -- user is retained if second_month_activity_ym is one month after first_month_activity_ym
+    -- comments: extra descriptions regarding user retention
+    ------------------------------------------------------------------------------------------------------------
 
-user_retention AS (    
-    -- description:
-    -- user [VARCHAR]
-    -- user's first active month [VARCHAR]
-    -- user's second active month [VARCHAR]
-    -- user is retained if second_active_month is one month after first_active_month, 1=RETAINED, 0=NOT RETAINED [INT]
-
-    SELECT 
+    select 
     user_id
-    , first_active_month
-    , second_active_month
-    , case when datediff('month', first_active_month, second_active_month) = 1 then 1 else 0 end user_retained_flag
+    , first_month_activity_ym
+    , second_month_activity_ym
+    , case when datediff('month', first_month_activity_ts, second_month_activity_ts) = 1 then 1 else 0 end user_retained_flag
+    , case 
+        when datediff('month', first_month_activity_ts, second_month_activity_ts) > 1 then 'user did not return within a month of first activity' 
+        when second_month_activity_ym is null then 'user has not returned since first month of activity'
+        else 'user retained, user was active in the month following first activity' 
+        end comments
 
-    FROM new_users_first_month a
-    LEFT JOIN new_users_second_month b USING (user_id)
-)
-
--- results read as:
--- XX.X% of users (retention_rate) who had their first activity in Month X (first_active_month) 
--- also showed activity in the subsequent month
-SELECT a.first_active_month, sum(a.user_retained_flag)/count(*) retention_rate
-FROM user_retention a
-WHERE first_active_month is not null
-GROUP BY 1
-ORDER BY 1
-;
+    from first_month_activity a
+    left join second_month_activity b using (user_id)
+),
 
 
-
-
-
-
--- TESTED ON FOLLOWING DDL
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-
--- use database testdb;
--- use schema lennar;
-
--- CREATE OR REPLACE TABLE user_activity_log (
---     user_id VARCHAR COMMENT 'Unique identifier for the user',
---     activity_type VARCHAR COMMENT 'Type of activity (e.g. "login", "purchase", "view_product", "add_to_cart")',
---     activity_timestamp TIMESTAMP_NTZ COMMENT 'Timestamp when the activity occurred',
---     product_id VARCHAR COMMENT 'Unique identifier for the product (relevant to activities "view_product", "add_to_cart", "purchase")',
---     activity_details VARIANT COMMENT 'Semi-structured JSON containing additional information about the activity (e.g., "price", "quantity", "page views")'
--- ) AS
-
--- select 1, 'view_product', to_timestamp(current_date()-30), 'PRODUCT_A', '{ "price": "1500", "quantity": "1" }'
--- union all select 1, 'view_product', to_timestamp(current_date()-29), 'PRODUCT_A', '{ "price": "1500", "quantity": "2" }'
--- union all select 1, 'add_to_cart', current_timestamp(), 'PRODUCT_A', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 2, 'view_product', to_timestamp(current_date()-30), 'PRODUCT_A', '{ "price": "1500", "quantity": "1" }'
--- union all select 2, 'view_product', to_timestamp(current_date()-29), 'PRODUCT_B', '{ "price": "1500", "quantity": "1" }'
--- union all select 2, 'add_to_cart', current_timestamp(), 'PRODUCT_A', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 3, 'view_product', to_timestamp(current_date()-30), 'PRODUCT_A', '{ "price": "1500", "quantity": "1" }'
--- union all select 3, 'view_product', to_timestamp(current_date()-29), 'PRODUCT_B', '{ "price": "1500", "quantity": "1" }'
--- union all select 3, 'add_to_cart', current_timestamp(), 'PRODUCT_B', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 4, 'view_product', to_timestamp(current_date()-30), 'PRODUCT_C', '{ "price": "1500", "quantity": "1" }'
--- union all select 4, 'add_to_cart', to_timestamp(current_date()-30), 'PRODUCT_C', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 5, 'view_product', to_timestamp(current_date()-30), 'PRODUCT_C', '{ "price": "1500", "quantity": "1" }'
--- union all select 5, 'add_to_cart', to_timestamp(current_date()-30), 'PRODUCT_C', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 6, 'view_product', to_timestamp(current_date()-30), 'PRODUCT_B', '{ "price": "1500", "quantity": "1" }'
--- union all select 6, 'add_to_cart', to_timestamp(current_date()-30), 'PRODUCT_B', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 7, 'view_product', to_timestamp(current_date()-60), 'PRODUCT_A', '{ "price": "1500", "quantity": "1" }'
--- union all select 7, 'add_to_cart', current_timestamp(), 'PRODUCT_A', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 8, 'view_product', to_timestamp(current_date()-60), 'PRODUCT_B', '{ "price": "1500", "quantity": "1" }'
--- union all select 8, 'add_to_cart', to_timestamp(current_date()-30), 'PRODUCT_B', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 9, 'view_product', to_timestamp(current_date()-60), 'PRODUCT_C', '{ "price": "1500", "quantity": "1" }'
--- union all select 9, 'add_to_cart', to_timestamp(current_date()-30), 'PRODUCT_C', '{ "price": "1500", "quantity": "2" }'
--- ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
------------------------------------- 
--- use database testdb;
--- use schema lennar;
-
--- CREATE OR REPLACE TABLE user_activity_log (
---     user_id VARCHAR COMMENT 'Unique identifier for the user',
---     activity_type VARCHAR COMMENT 'Type of activity (e.g. "login", "purchase", "view_product", "add_to_cart")',
---     activity_timestamp TIMESTAMP_NTZ COMMENT 'Timestamp when the activity occurred',
---     product_id VARCHAR COMMENT 'Unique identifier for the product (e.g. "view_product", "add_to_cart", "purchase")',
---     activity_details VARIANT COMMENT 'Semi-structured JSON containing additional information about the activity (e.g., "price", "quantity", "page views")'
--- ) AS
-
--- select 1, 'view_product', to_timestamp(current_date()-30), 'view_product', '{ "price": "1500", "quantity": "1" }'
--- union all select 1, 'add_to_cart', current_timestamp(), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 2, 'view_product', to_timestamp(current_date()-30), 'view_product', '{ "price": "1500", "quantity": "1" }'
--- union all select 2, 'add_to_cart', current_timestamp(), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 3, 'view_product', to_timestamp(current_date()-30), 'view_product', '{ "price": "1500", "quantity": "1" }'
--- union all select 3, 'add_to_cart', current_timestamp(), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 4, 'add_to_cart', to_timestamp(current_date()-30), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
--- union all select 5, 'add_to_cart', to_timestamp(current_date()-30), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
--- union all select 6, 'add_to_cart', to_timestamp(current_date()-30), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 7, 'view_product', to_timestamp(current_date()-60), 'view_product', '{ "price": "1500", "quantity": "1" }'
--- union all select 7, 'add_to_cart', current_timestamp(), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 8, 'view_product', to_timestamp(current_date()-60), 'view_product', '{ "price": "1500", "quantity": "1" }'
--- union all select 8, 'add_to_cart', to_timestamp(current_date()-30), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
-
--- union all select 9, 'view_product', to_timestamp(current_date()-60), 'view_product', '{ "price": "1500", "quantity": "1" }'
--- union all select 9, 'add_to_cart', to_timestamp(current_date()-30), 'add_to_cart', '{ "price": "1500", "quantity": "2" }'
--- ;
+select first_month_activity_ym, sum(a.user_retained_flag)/count(*) retention_rate
+from user_retention a
+group by 1
+order by 1
--- sql_problem/3_average_monthly_activities_per_user.sql
+++ sql_problem/3_average_monthly_activities_per_user.sql
@@ -1,35 +1,13 @@
+
 use database testdb;
 use schema lennar;
 
-with user_activity as (
-    select user_id, DATE_TRUNC('month',activity_timestamp) activity_month, count(activity_type) activity_cnt
-    from user_activity_log
-    group by 1, 2
-),
+select 
+DATE_TRUNC( 'month', activity_timestamp) activity_month_ts
+, count(distinct user_id) user_cnt                          -- count of unique users
+, count(*) total_activity_cnt                               -- total activity from all users
+, total_activity_cnt/user_cnt avg_activities_per_user       -- averaging total activity across users
 
-avg_monthly_activity as (
-    select activity_month, avg(activity_cnt) avg_activity_cnt
-    from user_activity
-    group by 1
-    order by 1 desc
-),
-
-user_avg_monthly_activity as (
-    select activity_month, user_id, avg(activity_cnt) avg_activity_cnt
-    from user_activity
-    group by 1, 2
-    order by 1 desc, 2
-),
-
-activities_per_unique_user as (
-    select activity_month
-    , sum(activity_cnt) total_activity
-    , count(distinct user_id) unique_active_users
-    , total_activity/unique_active_users total_activity_per_unique_user
-    from user_activity
-    group by 1
-    order by 1 desc
-)
-
-select * from activities_per_unique_user
-;
+from user_activity_log
+group by 1 
+order by 1 desc
