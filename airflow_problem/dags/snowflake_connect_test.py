e75ca760784379fdf88109563d4efd68c4e030e4
Jackson Hoyt <jackson.t.hoyt@gmail.com>
2024-06-23 21:22:20 -0500

    Adding python snowflake connector

--- airflow_problem/dags/snowflake_connect_test.py
+++ airflow_problem/dags/snowflake_connect_test.py
@@ -1,13 +1,27 @@
 import os
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from airflow.decorators import task
 from astro import sql as aql
+import snowflake.connector
 
 @aql.run_raw_sql
 def aql_snowflake_connection_test():
     return 'SELECT CURRENT_DATE()'
+
+def snowflake_connetor():
+    with snowflake.connector.connect(
+        user=os.environ['SNOWFLAKE_USER'],
+        password=os.environ['SNOWFLAKE_PW'],
+        account=f"{os.environ['SNOWFLAKE_ACCT']}.{os.environ['SNOWFLAKE_REGION']}.aws",
+        warehouse=os.environ['SNOWFLAKE_WH'],
+        database=os.environ['SNOWFLAKE_DB'],
+        schema=os.environ['SNOWFLAKE_SCHEMA']
+    ) as con:
+        cursor = con.cursor()
+        return cursor.execute('SELECT CURRENT_DATE()').fetchall()
 
 # Define default args
 default_args = {
@@ -29,6 +43,15 @@ with DAG(
     tags=["example"],
 ):
    
+    snowflake_connetor_task = PythonOperator(
+        task_id='snowflake_connetor',
+        python_callable=snowflake_connetor,
+        op_kwargs={
+            'table_name': 'YOUR_TABLE_NAME',
+            'datetime_col': 'YOUR_DATETIME_COLUMN'
+        },
+    )
+
     # https://www.astronomer.io/docs/learn/astro-python-sdk-etl#before-and-after-the-astro-python-sdk
     aql_snowflake_connection_test = aql_snowflake_connection_test(conn_id=os.environ['SNOWFLAKE_CONN'])
 
